c21b113782b571322a9e1ba67604c445fc05d3e3
sayas44 <saumiyahya17@gmail.com>
2025-10-02 13:32:28 +0800

    tugas 1

--- jobsheet5/ifCetakKRS28.java
+++ jobsheet5/ifCetakKRS28.java
@@ -7,7 +7,10 @@ public class ifCetakKRS28 {
         System.out.println("---Cetak KRS SIAKAD---");
         System.out.print("apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-
+        String pesan;
+        pesan = uktLunas ? "pembayaran UKTterverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA" : "\n";
+        System.out.println(pesan);
+        
         if (uktLunas) {
             System.out.println("pembayaran UKTterverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
